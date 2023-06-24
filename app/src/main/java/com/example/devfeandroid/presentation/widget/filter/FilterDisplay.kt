fc9445d96bc0aafe0014b458cacef03ffff0e4d9
tunglv <tunglv@ftech.ai>
2023-06-25 00:19:38 +0700

    left view

--- app/src/main/java/com/example/devfeandroid/presentation/widget/filter/FilterDisplay.kt
+++ app/src/main/java/com/example/devfeandroid/presentation/widget/filter/FilterDisplay.kt
@@ -15,6 +15,19 @@ class FilterDisplay : IFilterDisplay {
                 list.add(FilterData(title = getAppString(R.string.now), isSelect = false))
                 list.add(FilterData(title = getAppString(R.string.follow), isSelect = false))
             }
+
+            FILTER_TYPE.STORE -> {
+                list.add(FilterData(title = getAppString(R.string.home), isSelect = true))
+                list.add(FilterData(title = getAppString(R.string.category), isSelect = false))
+                list.add(FilterData(title = getAppString(R.string.review), isSelect = false))
+                list.add(FilterData(title = getAppString(R.string.theme), isSelect = false))
+                list.add(FilterData(title = getAppString(R.string.brand), isSelect = false))
+                list.add(FilterData(title = getAppString(R.string.other1), isSelect = false))
+                list.add(FilterData(title = getAppString(R.string.other2), isSelect = false))
+                list.add(FilterData(title = getAppString(R.string.other3), isSelect = false))
+                list.add(FilterData(title = getAppString(R.string.other4), isSelect = false))
+            }
+
             else -> arrayListOf<FilterData>()
         }
         return list
--- app/src/main/java/com/example/devfeandroid/presentation/widget/filter/data/FILTER_TYPE.kt
+++ app/src/main/java/com/example/devfeandroid/presentation/widget/filter/data/FILTER_TYPE.kt
@@ -1,7 +1,7 @@
 package com.example.devfeandroid.presentation.widget.filter.data
 
 enum class FILTER_TYPE(val value: Int) {
-    HOME(0);
+    HOME(0), STORE(1);
 
     companion object {
         fun valueOfName(type: Int): FILTER_TYPE? {
